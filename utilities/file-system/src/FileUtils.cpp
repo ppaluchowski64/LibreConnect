b62633509abfc2ce22a63959e64afa6e4f35739c
Pablo Majster <104451887+ppaluchowski64@users.noreply.github.com>
2026-01-08 20:38:15 +0100

    Fix build workflow issue
    Updated runner versions to newer, current, and recommended ones to ensure the use of tools that fully meet the latest standards. In this case, this refers to C++20, which was not fully supported by the previous compiler versions and could cause compilation errors with less standard syntax.
    
    P.S. The final fix also adjusts the way file modification time is obtained on macOS, as the previously used approach is not yet supported by libc++ on that platform.

--- utilities/file-system/src/FileUtils.cpp
+++ utilities/file-system/src/FileUtils.cpp
@@ -100,8 +100,13 @@ std::optional<FileType> DetectFileType(const std::filesystem::path& path) {
 
 std::optional<std::time_t> GetFileLastModTime(const std::filesystem::path& path) {
     try {
-        auto ftime = std::filesystem::last_write_time(path);
-        auto sctp = std::chrono::clock_cast<std::chrono::system_clock>(ftime);
+        const auto ftime = std::filesystem::last_write_time(path);
+
+        const auto sctp = std::chrono::time_point_cast<std::chrono::system_clock::duration>(
+            ftime - std::filesystem::file_time_type::clock::now()
+            + std::chrono::system_clock::now()
+        );
+
         return std::chrono::system_clock::to_time_t(sctp);
     } catch (const std::filesystem::filesystem_error&) {
         return std::nullopt;
