b3f9bb9565eca7407735c96ede5c2bf771a34d2c
ddj4747 <139778357+ddj4747@users.noreply.github.com>
2026-03-04 21:37:48 +0100

    Threadpool Stop/Start functions

--- utilities/network/src/ThreadPool.cpp
+++ utilities/network/src/ThreadPool.cpp
@@ -8,19 +8,39 @@ void ThreadPool::Initialize() {
 }
 
 ThreadPool::ThreadPool() : m_workGuard(asio::make_work_guard(m_context)){
-    unsigned int threadCount = std::thread::hardware_concurrency();
-    if (threadCount == 0) threadCount = 1;
-
-    m_threads.reserve(threadCount);
-
-    for (int i = 0; i < threadCount; i++) {
-        m_threads.emplace_back([this]() {
-            m_context.run();
-        });
-    }
+    Start();
 }
 
 IOContext& ThreadPool::GetContext() {
     std::call_once(s_flag, Initialize);
     return s_instance->m_context;
+}
+
+void ThreadPool::Stop() {
+    std::call_once(s_flag, Initialize);
+
+    s_instance->m_workGuard.reset();
+    s_instance->m_context.stop();
+
+    for (auto& thread : s_instance->m_threads) {
+        if (thread.joinable()) {
+            thread.join();
+        }
+    }
+
+    s_instance->m_threads.clear();
+}
+
+void ThreadPool::Start() {
+    std::call_once(s_flag, Initialize);
+    unsigned int threadCount = std::thread::hardware_concurrency();
+    if (threadCount == 0) threadCount = 1;
+
+    s_instance->m_threads.reserve(threadCount);
+
+    for (int i = 0; i < threadCount; i++) {
+        s_instance->m_threads.emplace_back([]() {
+            s_instance->m_context.run();
+        });
+    }
 }
