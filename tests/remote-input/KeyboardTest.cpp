0dfd3c9584ec619e85c4d6d9997ff537886243ae
Pablo Majster <104451887+ppaluchowski64@users.noreply.github.com>
2026-04-13 21:45:02 +0200

    Unify remote keyboard API with cross-platform abstraction
    Refactored the keyboard API to use a unified Key enum class and created a mapping function to translate these keys into native OS codes for Linux, macOS, and Windows. Updated all platform implementations to use this new abstraction instead of raw integers. Simplified the test program by removing platform-specific macros to make it fully independent of the underlying operating system.

--- tests/remote-input/KeyboardTest.cpp
+++ tests/remote-input/KeyboardTest.cpp
@@ -4,18 +4,6 @@
 #include <thread>
 #include <chrono>
 
-#ifdef __linux__
-    #include <linux/input-event-codes.h>
-#endif
-
-#ifdef _WIN32
-    #include <Windows.h>
-#endif
-
-#ifdef __APPLE__
-    #include <Carbon/Carbon.h>
-#endif
-
 int main() {
     try {
         Keyboard kb;
@@ -23,57 +11,21 @@ int main() {
         std::cout << "You have 5 seconds to focus a text input (e.g. Notepad)...\n";
         std::this_thread::sleep_for(std::chrono::seconds(5));
 
-        #ifdef __linux__
+        kb.PressKey(Key::LeftShift); kb.PressAndReleaseKey(Key::H); kb.ReleaseKey(Key::LeftShift);
+        kb.PressAndReleaseKey(Key::E);
+        kb.PressAndReleaseKey(Key::L);
+        kb.PressAndReleaseKey(Key::L);
+        kb.PressAndReleaseKey(Key::O);
 
-            kb.PressKey(KEY_LEFTSHIFT); kb.PressAndReleaseKey(KEY_H); kb.ReleaseKey(KEY_LEFTSHIFT);
-            kb.PressAndReleaseKey(KEY_E);
-            kb.PressAndReleaseKey(KEY_L);
-            kb.PressAndReleaseKey(KEY_L);
-            kb.PressAndReleaseKey(KEY_O);
-            kb.PressAndReleaseKey(KEY_SPACE);
-            kb.PressKey(KEY_LEFTSHIFT); kb.PressAndReleaseKey(KEY_W); kb.ReleaseKey(KEY_LEFTSHIFT);
-            kb.PressAndReleaseKey(KEY_O);
-            kb.PressAndReleaseKey(KEY_R);
-            kb.PressAndReleaseKey(KEY_L);
-            kb.PressAndReleaseKey(KEY_D);
-            kb.PressKey(KEY_LEFTSHIFT); kb.PressAndReleaseKey(KEY_1); kb.ReleaseKey(KEY_LEFTSHIFT);
+        kb.PressAndReleaseKey(Key::Space);
 
-        #elif __APPLE__
+        kb.PressKey(Key::LeftShift); kb.PressAndReleaseKey(Key::W); kb.ReleaseKey(Key::LeftShift);
+        kb.PressAndReleaseKey(Key::O);
+        kb.PressAndReleaseKey(Key::R);
+        kb.PressAndReleaseKey(Key::L);
+        kb.PressAndReleaseKey(Key::D);
 
-            kb.PressKey(kVK_Shift); kb.PressAndReleaseKey(kVK_ANSI_H); kb.ReleaseKey(kVK_Shift);
-            kb.PressAndReleaseKey(kVK_ANSI_E);
-            kb.PressAndReleaseKey(kVK_ANSI_L);
-            kb.PressAndReleaseKey(kVK_ANSI_L);
-            kb.PressAndReleaseKey(kVK_ANSI_O);
-            kb.PressAndReleaseKey(kVK_Space);
-            kb.PressKey(kVK_Shift); kb.PressAndReleaseKey(kVK_ANSI_W); kb.ReleaseKey(kVK_Shift);
-            kb.PressAndReleaseKey(kVK_ANSI_O);
-            kb.PressAndReleaseKey(kVK_ANSI_R);
-            kb.PressAndReleaseKey(kVK_ANSI_L);
-            kb.PressAndReleaseKey(kVK_ANSI_D);
-            kb.PressKey(kVK_Shift); kb.PressAndReleaseKey(kVK_ANSI_1); kb.ReleaseKey(kVK_Shift);
-
-        #elif _WIN32
-
-            kb.PressKey(VK_SHIFT); kb.PressAndReleaseKey('H'); kb.ReleaseKey(VK_SHIFT);
-            kb.PressAndReleaseKey('E');
-            kb.PressAndReleaseKey('L');
-            kb.PressAndReleaseKey('L');
-            kb.PressAndReleaseKey('O');
-            kb.PressAndReleaseKey(VK_SPACE);
-            kb.PressKey(VK_SHIFT); kb.PressAndReleaseKey('W'); kb.ReleaseKey(VK_SHIFT);
-            kb.PressAndReleaseKey('O');
-            kb.PressAndReleaseKey('R');
-            kb.PressAndReleaseKey('L');
-            kb.PressAndReleaseKey('D');
-            kb.PressKey(VK_SHIFT); kb.PressAndReleaseKey('1'); kb.ReleaseKey(VK_SHIFT);
-
-        #else
-
-            std::cerr << "Not supported platform!\n";
-            return 1;
-
-        #endif
+        kb.PressKey(Key::LeftShift); kb.PressAndReleaseKey(Key::Num1); kb.ReleaseKey(Key::LeftShift);
 
         return 0;
     } catch (const std::exception& e) {
--- utilities/remote-input/inc/Keyboard.h
+++ utilities/remote-input/inc/Keyboard.h
@@ -1,11 +1,15 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
+#include <cstdint>
+
 #ifdef __APPLE__
+    #include <AwaitableFlag.h>
+
     #include <asio.hpp>
     #include <asio/awaitable.hpp>
     #include <concurrentqueue.h>
-    #include <AwaitableFlag.h>
+
     #include <atomic>
 
     struct KeyEvent {
@@ -14,14 +18,77 @@
     };
 #endif
 
+enum class Key : uint8_t {
+    Unknown = 0,
+
+    A, B, C, D, E, F, G, H, I, J, K, L, M,
+    N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
+
+    Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
+
+    Grave,        // ` ~
+    Minus,        // - _
+    Equal,        // = +
+    LeftBracket,  // [ {
+    RightBracket, // ] }
+    Backslash,    // \ |
+    Semicolon,    // ; :
+    Apostrophe,   // ' "
+    Comma,        // , <
+    Period,       // . >
+    Slash,        // / ?
+
+    Escape,
+    Tab,
+    CapsLock,
+    Space,
+    Backspace,
+    Enter,
+
+    LeftShift, RightShift,
+    LeftControl, RightControl,
+    LeftAlt, RightAlt,
+    LeftSuper, RightSuper, // Win / Cmd / Meta
+    Menu,
+
+    F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
+    F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24,
+
+    PrintScreen,
+    ScrollLock,
+    Pause,
+
+    Insert,
+    Delete,
+    Home,
+    End,
+    PageUp,
+    PageDown,
+
+    Up,
+    Down,
+    Left,
+    Right,
+
+    NumLock,
+    Numpad0, Numpad1, Numpad2, Numpad3, Numpad4,
+    Numpad5, Numpad6, Numpad7, Numpad8, Numpad9,
+    NumpadAdd,
+    NumpadSubtract,
+    NumpadMultiply,
+    NumpadDivide,
+    NumpadDecimal,
+    NumpadEnter
+};
+
 class Keyboard {
     public:
         Keyboard();
         ~Keyboard();
 
-        void PressKey(int keyCode);
-        void ReleaseKey(int keyCode);
-        void PressAndReleaseKey(int keyCode);
+        void PressKey(Key key);
+        void ReleaseKey(Key key);
+        void PressAndReleaseKey(Key key);
 
     #ifdef __APPLE__
     private:
