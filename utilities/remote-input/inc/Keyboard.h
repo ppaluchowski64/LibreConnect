c83bfe49524b7e5655a94eb692f564578dc5eda9
Pablo Majster <104451887+ppaluchowski64@users.noreply.github.com>
2026-04-15 20:47:37 +0200

    Fix Android build error and refactor remote input
    Fixed the Android compilation failure by removing the desktop-only Keyboard header dependency from mobile module. Extracted shared input types into a separate library and updated the build configuration to use it for mobile targets.

--- utilities/remote-input/inc/Keyboard.h
+++ utilities/remote-input/inc/Keyboard.h
@@ -1,7 +1,7 @@
 #ifndef KEYBOARD_H
 #define KEYBOARD_H
 
-#include <cstdint>
+#include "InputTypes.h"
 
 #ifdef __linux__
 
@@ -25,69 +25,6 @@
     };
 
 #endif
-
-enum class Key : uint8_t {
-    Unknown = 0,
-
-    A, B, C, D, E, F, G, H, I, J, K, L, M,
-    N, O, P, Q, R, S, T, U, V, W, X, Y, Z,
-
-    Num0, Num1, Num2, Num3, Num4, Num5, Num6, Num7, Num8, Num9,
-
-    Grave,        // ` ~
-    Minus,        // - _
-    Equal,        // = +
-    LeftBracket,  // [ {
-    RightBracket, // ] }
-    Backslash,    // \ |
-    Semicolon,    // ; :
-    Apostrophe,   // ' "
-    Comma,        // , <
-    Period,       // . >
-    Slash,        // / ?
-
-    Escape,
-    Tab,
-    CapsLock,
-    Space,
-    Backspace,
-    Enter,
-
-    LeftShift, RightShift,
-    LeftControl, RightControl,
-    LeftAlt, RightAlt,
-    LeftSuper, RightSuper, // Win / Cmd / Meta
-    Menu,
-
-    F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12,
-    F13, F14, F15, F16, F17, F18, F19, F20, F21, F22, F23, F24,
-
-    PrintScreen,
-    ScrollLock,
-    Pause,
-
-    Insert,
-    Delete,
-    Home,
-    End,
-    PageUp,
-    PageDown,
-
-    Up,
-    Down,
-    Left,
-    Right,
-
-    NumLock,
-    Numpad0, Numpad1, Numpad2, Numpad3, Numpad4,
-    Numpad5, Numpad6, Numpad7, Numpad8, Numpad9,
-    NumpadAdd,
-    NumpadSubtract,
-    NumpadMultiply,
-    NumpadDivide,
-    NumpadDecimal,
-    NumpadEnter
-};
 
 class Keyboard {
     public:
