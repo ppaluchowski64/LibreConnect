779c4195b7a9d8967abf72f762759037eb16fe6c
tapyk <152629297+tapyk@users.noreply.github.com>
2026-04-22 11:10:25 +0200

    Fix Android sync clipboard tile

--- android/src/com/LibreConnect/mobile/ClipboardTileService.kt
+++ android/src/com/LibreConnect/mobile/ClipboardTileService.kt
@@ -1,14 +1,36 @@
 package com.LibreConnect.mobile
 
+import android.os.Build
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.util.Log
 
 class ClipboardTileService : TileService() {
+    override fun onTileAdded() {
+        super.onTileAdded()
+        Log.i(TAG, "Clipboard tile added")
+        refreshTile()
+    }
+
+    override fun onStartListening() {
+        super.onStartListening()
+        refreshTile()
+    }
 
     override fun onClick() {
         super.onClick()
         Log.i(TAG, "Clipboard tile clicked")
+        refreshTile()
         startActivityAndCollapse(ClipboardActionActivity.createLaunchIntent(this))
+    }
+
+    private fun refreshTile() {
+        val tile = qsTile ?: return
+        tile.state = Tile.STATE_ACTIVE
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            tile.subtitle = "Send clipboard"
+        }
+        tile.updateTile()
     }
 
     companion object {
